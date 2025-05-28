059672dc065c77d107bc9c17d0ee5864bfa88ed4
adkiw <DARIUS.AUKSELIS@GMAIL.COM>
2025-05-28 15:31:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
 import streamlit as st
+from db import init_db
 
+# Importuoji modulius (privalo būti failai: modules/vilkikai.py ir t.t.)
+from modules import (
+    vilkikai,
+    priekabos,
+    kroviniai,
+    darbuotojai,
+    klientai,
+    grupes,
+    nustatymai
+)
+
+# Nustatom Streamlit puslapio konfigūraciją
 st.set_page_config(layout="wide")
-st.title("DISPO sistema – pagrindinis langas")
+conn, c = init_db()
 
-st.markdown("Pasirink modulį kairėje pusėje.")
+# Šoninis meniu
+moduliai = [
+    "DISPO", "Vilkikai", "Priekabos",
+    "Kroviniai", "Darbuotojai", "Klientai",
+    "Grupės", "Nustatymai"
+]
+modulis = st.sidebar.radio("📂 Pasirink modulį", moduliai)
+
+# Pagrindinio turinio vaizdavimas
+if modulis == "DISPO":
+    st.title("DISPO sistema – pagrindinis langas")
+    st.write("Pasirink modulį kairėje pusėje.")
+elif modulis == "Vilkikai":
+    vilkikai.show(conn, c)
+elif modulis == "Priekabos":
+    priekabos.show(conn, c)
+elif modulis == "Kroviniai":
+    kroviniai.show(conn, c)
+elif modulis == "Darbuotojai":
+    darbuotojai.show(conn, c)
+elif modulis == "Klientai":
+    klientai.show(conn, c)
+elif modulis == "Grupės":
+    grupes.show(conn, c)
+elif modulis == "Nustatymai":
+    nustatymai.show(conn, c)
